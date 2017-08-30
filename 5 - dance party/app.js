8cb3540540bf9e956c2add5387b284b36a085e36
Cameron Earl <developer.cameron@gmail.com>
2017-08-30 14:33:06 -0700

    Complete ex 5

--- 5 - dance party/app.js
+++ 5 - dance party/app.js
@@ -6,5 +6,14 @@ var colorArr = ["red", "blue", "green", "purple", "pink", "black"]
 function startDance () {
   // Every .2 seconds change the colors of all the divs with the "color" class.
   // stretch goal - randomly change the size of them as well!
-  // your code here
+  window.setInterval(()=> {
+    let divs = document.querySelectorAll(".color");
+    for (let div of divs) {
+      let rColor = colorArr[Math.floor(Math.random() * colorArr.length)];
+      div.style.backgroundColor = rColor;
+      let size = Math.floor(Math.random() * 200) + "px";
+      div.style.width = size;
+      div.style.height = size;
+    }
+  },200);
 }
