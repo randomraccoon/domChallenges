97a17dabd921a17e2e325bc3e76b73df18f41caf
Cameron Earl <developer.cameron@gmail.com>
2017-08-30 13:59:12 -0700

    Complete ex 1

--- 1 - changing colors/app.js
+++ 1 - changing colors/app.js
@@ -4,4 +4,10 @@ document.body.onload = changeColors;
 function changeColors () {
   // Change the color of the header text to red and change the color of the p tags to blue and the font size of the p tags to 10px.
   // your code here
+  document.querySelector("h4").style.color = "red";
+  let pArr = document.querySelectorAll("p");
+  for (let p of pArr) {
+    p.style.color = "blue";
+    p.style.fontSize = "10px";
+  }
 }
