7849730973fac52120d23c10cd061333c10eadf4
Cameron Earl <developer.cameron@gmail.com>
2017-08-30 14:11:32 -0700

    Complete ex 2

--- 2 - create blocks/app.js
+++ 2 - create blocks/app.js
@@ -3,6 +3,13 @@ document.body.onload = genBlocks;
 
 function genBlocks() {
   // Create 10 divs with the classes 'red' and 'black'. Interchange these to create a checkerboard pattern.
-  // your code here
+  let isRed = true;
+  for (let i = 0; i < 10; i++) {
+    let color = isRed ?  "red" : "black";
+    let el = document.createElement("div");
+    el.classList.add(color);
+    document.body.appendChild(el);
+    isRed = !isRed;
+  }
 
 }
